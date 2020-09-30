9332aa9101a500e5fac5fde2549fe7d89d049996
James Capps <jcapps@protonmail.com>
2020-09-30 15:19:55 -0500

    Removed redundant variables

--- server/process.php
+++ server/process.php
@@ -40,20 +40,6 @@
         // Need to adjust for all seasons
         public function addGame() {
 
-            $date     = $_POST["date"];
-            $week     = $_POST["week"];
-            $opp      = $_POST["opp"];
-            $result   = $_POST["result"];
-            $score    = $_POST["score"];
-            $comp     = $_POST["comp"];
-            $att      = $_POST["att"];
-            $compPerc = $_POST["compPerc"];
-            $yds      = $_POST["yds"];
-            $td       = $_POST["td"];
-            $ints      = $_POST["int"];
-            $rate     = $_POST["rate"];
-            $rushyds  = $_POST["rushyds"];
-
             $db  = new Connect;
             $sql = $db -> prepare("INSERT INTO twentytwenty (
                                     date, 
@@ -72,19 +58,19 @@
                                  )
                                  VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 
-            $sql -> bindParam(1,  $date,     PDO::PARAM_STR);
-            $sql -> bindParam(2,  $week,     PDO::PARAM_STR);
-            $sql -> bindParam(3,  $opp,      PDO::PARAM_STR);
-            $sql -> bindParam(4,  $result,   PDO::PARAM_STR);
-            $sql -> bindParam(5,  $score,    PDO::PARAM_STR);
-            $sql -> bindParam(6,  $att,      PDO::PARAM_INT);
-            $sql -> bindParam(7,  $comp   ,  PDO::PARAM_INT);
-            $sql -> bindParam(8,  $compPerc, PDO::PARAM_INT);
-            $sql -> bindParam(9,  $yds,      PDO::PARAM_INT);
-            $sql -> bindParam(10, $td,       PDO::PARAM_INT);
-            $sql -> bindParam(11, $ints,     PDO::PARAM_INT);
-            $sql -> bindParam(12, $rate ,    PDO::PARAM_STR);
-            $sql -> bindParam(13, $rushyds,  PDO::PARAM_INT);
+            $sql -> bindParam(1,  $_POST["date"],     PDO::PARAM_STR);
+            $sql -> bindParam(2,  $_POST["week"],     PDO::PARAM_STR);
+            $sql -> bindParam(3,  $_POST["opp"],      PDO::PARAM_STR);
+            $sql -> bindParam(4,  $_POST["result"],   PDO::PARAM_STR);
+            $sql -> bindParam(5,  $_POST["score"],    PDO::PARAM_STR);
+            $sql -> bindParam(6,  $_POST["att"],      PDO::PARAM_INT);
+            $sql -> bindParam(7,  $_POST["comp"]   ,  PDO::PARAM_INT);
+            $sql -> bindParam(8,  $_POST["compPerc"], PDO::PARAM_INT);
+            $sql -> bindParam(9,  $_POST["yds"],      PDO::PARAM_INT);
+            $sql -> bindParam(10, $_POST["td"],       PDO::PARAM_INT);
+            $sql -> bindParam(11, $_POST["int"],     PDO::PARAM_INT);
+            $sql -> bindParam(12, $_POST["rate"] ,    PDO::PARAM_STR);
+            $sql -> bindParam(13, $_POST["rushyds"],  PDO::PARAM_INT);
 
             try {
                 $sql -> execute();
@@ -97,21 +83,6 @@
 
         // Need to adjust for all seasons
         public function updateGame() {
-
-            $id       = $_POST["id"];
-            $date     = $_POST["date"];
-            $week     = $_POST["week"];
-            $opp      = $_POST["opp"];
-            $result   = $_POST["result"];
-            $score    = $_POST["score"];
-            $comp     = $_POST["comp"];
-            $att      = $_POST["att"];
-            $compPerc = $_POST["compPerc"];
-            $yds      = $_POST["yds"];
-            $td       = $_POST["td"];
-            $ints      = $_POST["int"];
-            $rate     = $_POST["rate"];
-            $rushyds  = $_POST["rushyds"];
 
             $db = new Connect;
             $sql = $db -> prepare("UPDATE twentytwenty SET
@@ -131,20 +102,20 @@
                                  WHERE id = ?
                                  ");
 
-            $sql -> bindParam(1,  $date,     PDO::PARAM_STR);
-            $sql -> bindParam(2,  $week,     PDO::PARAM_STR);
-            $sql -> bindParam(3,  $opp,      PDO::PARAM_STR);
-            $sql -> bindParam(4,  $result,   PDO::PARAM_STR);
-            $sql -> bindParam(5,  $score,    PDO::PARAM_STR);
-            $sql -> bindParam(6,  $att,      PDO::PARAM_INT);
-            $sql -> bindParam(7,  $comp   ,  PDO::PARAM_INT);
-            $sql -> bindParam(8,  $compPerc, PDO::PARAM_INT);
-            $sql -> bindParam(9,  $yds,      PDO::PARAM_INT);
-            $sql -> bindParam(10, $td,       PDO::PARAM_INT);
-            $sql -> bindParam(11, $ints,     PDO::PARAM_INT);
-            $sql -> bindParam(12, $rate ,    PDO::PARAM_STR);
-            $sql -> bindParam(13, $rushyds,  PDO::PARAM_INT);
-            $sql -> bindParam(14, $id,       PDO::PARAM_INT);
+            $sql -> bindParam(1,  $_POST["date"],     PDO::PARAM_STR);
+            $sql -> bindParam(2,  $_POST["week"],     PDO::PARAM_STR);
+            $sql -> bindParam(3,  $_POST["opp"],      PDO::PARAM_STR);
+            $sql -> bindParam(4,  $_POST["result"],   PDO::PARAM_STR);
+            $sql -> bindParam(5,  $_POST["score"],    PDO::PARAM_STR);
+            $sql -> bindParam(6,  $_POST["att"],      PDO::PARAM_INT);
+            $sql -> bindParam(7,  $_POST["comp"]   ,  PDO::PARAM_INT);
+            $sql -> bindParam(8,  $_POST["compPerc"], PDO::PARAM_INT);
+            $sql -> bindParam(9,  $_POST["yds"],      PDO::PARAM_INT);
+            $sql -> bindParam(10, $_POST["td"],       PDO::PARAM_INT);
+            $sql -> bindParam(11, $_POST["int"],     PDO::PARAM_INT);
+            $sql -> bindParam(12, $_POST["rate"] ,    PDO::PARAM_STR);
+            $sql -> bindParam(13, $_POST["rushyds"],  PDO::PARAM_INT);
+            $sql -> bindParam(14, $_POST["id"],       PDO::PARAM_INT);
 
             try {
                 $sql -> execute();
